0620d0bb4655cebd1755c6d58188b0b652b4fcba
Reda Elmountassir <55627855+redaelmountassir@users.noreply.github.com>
2022-06-28 13:41:07 -0400

    Update PostForm.js

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,8 +1,8 @@
 function PostForm() {
 	return (
 		<form>
-			<input name='Name' type='text' placeholder='Name' />
-			<input name='Title' type='text' placeholder='Title' />
+			<input name='name' type='text' placeholder='Name' />
+			<input name='title' type='text' placeholder='Title' />
 			<textarea name='text' cols='30' rows='10' placeholder='Text'></textarea>
 			<button>Submit</button>
 		</form>
