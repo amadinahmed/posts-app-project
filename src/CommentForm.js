31d6cfbd5870d49f814f65fda06eb2c021462c4a
Reda Elmountassir <redaelmountassir0@gmail.com>
2022-06-28 13:11:16 -0400

    Converted divs to forms

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,10 +1,10 @@
 function CommentForm() {
 	return (
-		<div>
+		<form>
 			<input type='text' placeholder='Name' />
 			<textarea name='text' cols='30' rows='10' placeholder='Text'></textarea>
 			<button>Submit</button>
-		</div>
+		</form>
 	);
 }
 
--- src/PostForm.js
+++ src/PostForm.js
@@ -1,11 +1,11 @@
 function PostForm() {
 	return (
-		<div>
+		<form>
 			<input type='text' placeholder='Name' />
 			<input type='text' placeholder='Title' />
 			<textarea name='text' cols='30' rows='10' placeholder='Text'></textarea>
 			<button>Submit</button>
-		</div>
+		</form>
 	);
 }
 
