903f88d04e593d26c9f98c4f3736fe57aa22e4a2
Reda Elmountassir <55627855+redaelmountassir@users.noreply.github.com>
2022-06-28 13:41:30 -0400

    Update CommentForm.js

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,7 +1,7 @@
 function CommentForm() {
 	return (
 		<form>
-			<input type='text' placeholder='Name' />
+			<input name='name' type='text' placeholder='Name' />
 			<textarea name='text' cols='30' rows='10' placeholder='Text'></textarea>
 			<button>Submit</button>
 		</form>
